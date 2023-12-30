8241421975f4227b30b4175b0b83342e34584aeb
Snow0wl <des00012@mix.wvu.edu>
2023-12-29 23:14:01 -0500

    added species to characterCreation

--- index.js
+++ index.js
@@ -1,135 +1,212 @@
-import humanoid from './humanoid.json' assert { type: 'json' };
+import humanoidCharacterStats from './humanoid.json' assert { type: 'json' };
+import HumanoidSpecies from './humanoidSpeciesList.json' assert { type: 'json' };
 import generalCharacterStats from './generalCharacterStats.json' assert { type: 'json' };
 
-
-
-function randomLine(id) {
-    switch (true) {
-        case id == "name":
-            var options = humanoid.nameList;
-            break;
-        case id == "type":
-            var options = humanoid.typeList;
-            break;
-        case id == "superpowers":
-            var options = generalCharacterStats.superList;
-            break;
-        case id == "superpower2":
-            var options = generalCharacterStats.superList;
-            break;
-        case id == "age":
-            var options = humanoid.ageRange;
-            return Math.floor(Math.random() * options);
-        case id == "sex":
-            var options = humanoid.sexesList;
-            break;
-        case id == "sexuality":
-            var options = humanoid.sexualityList;
-            break;
-        case id == "experiance":
-            var options = generalCharacterStats.proficiencies;
-            break;
-        case id == "positive":
-            var options = generalCharacterStats.personality.positiveList;
-            break;
-        case id == "negative":
-            var options = generalCharacterStats.personality.negativeList;
-            break;
-        case id == "neutral":
-            var options = generalCharacterStats.personality.neutralList;
-            break;
-        case id == "ideology":
-            var options = humanoid.ideologyList;
-            break;
-    }
-    var element = Math.floor(Math.random() * options.length);
-    return options[element];
+function humanoidToList(species) {
+    var options = HumanoidSpecies[species];
+    console.log(options);
+    return options;
 }
-export function returnElement(id){
-    if(id === "personality"){
-        let traitList = Array();
-        let maxChance = 100;
-        for(let i=0;i<3;i++){
-            var chance = Math.random() * maxChance;
-            switch (true) {
-                case chance < 18:
-                    var newID = "neutral";
-                    break;
-                case chance < 54:
-                    var newID = "positive";
-                    break;
-                case 53 < chance:
-                    var newID = "negative";
-                    break;
-            }
-            traitList.push(randomLine(newID));
-        }
-        var string = "";
-        traitList.forEach(function (element) {
-            string += element + " ";
-        });
-        document.getElementById(id).innerText = string;
+
+function returnSpecies(character, species) {
+    switch (true) {
+        case character == "humanoid":
+            var listFunction = humanoidToList(species);
+        // case character == "mystic":
+        //     var listFunction = humanoidToList(id);
+        // case character == "alien":
+        //     var listFunction = humanoidToList(id);
+        // case character == "creature":
+        //     var listFunction = humanoidToList(id);
+    }
+    var list = listFunction;
+    var species = randomLine(list);
+    document.getElementById("speciesList").innerText = species;
+    return species;
+}
+
+function humanoidSpeciesToList(species,id) {
+    var options = humanoidCharacterStats.humanoid[species][id];
+    return options;
+}
+
+function generalToList(id) {
+    switch (true) {
+        case id == "superList":
+            var options = generalCharacterStats[id];
+            break;
+        case id == "proficiencyList":
+            var options = generalCharacterStats[id];
+            break;
+        case id == "positiveList":
+            var options = generalCharacterStats["personality"][id];
+            break;
+        case id == "negativeList":
+            var options = generalCharacterStats["personality"][id];
+            break;
+        case id == "neutralList":
+            var options = generalCharacterStats["personality"][id];
+            break;
+    }
+    return options;
+}
+
+function randomLine(list) {
+
+    var element = Math.floor(Math.random() * list.length);
+    return list[element];
+}
+function returnElement(character, species, id) {
+    switch (true) {
+        case character == "humanoid":
+            var listFunction = humanoidSpeciesToList(species,id);
+        case character == "mystic":
+            var listFunction = humanoidSpeciesToList(species,id);
+        case character == "alien":
+            var listFunction = humanoidSpeciesToList(species,id);
+        case character == "creature":
+            var listFunction = humanoidSpeciesToList(species,id);
+    }
+    if (id === "personality") {
+        document.getElementById(id).innerText = getPersonality();
         return;
     }
-    if(id === "experiance"){
-        let traitList = Array();
-        for(let i=0;i<3;i++){
-            traitList.push(randomLine(id));
-        }
-        var string = "";
-        traitList.forEach(function (element) {
-            string += element +" ";
-        });
-        document.getElementById(id).innerText = string;
+    if (id === "proficiencyList") {
+        document.getElementById(id).innerText = getProficiencies(id);
         return;
     }
-    else{
-        document.getElementById(id).innerText = randomLine(id);
+    if (id === "ageRange") {
+        var list = listFunction;
+        var age = Math.floor(Math.random() * list);
+        document.getElementById(id).innerText = age;
+        return age;
     }
-}    
+    if (id === "superList2") {
+        var newID = id.substring(0, id.length - 1);
+        var list = generalToList(newID);
+        document.getElementById(id).innerText = randomLine(list);
+        return;
+    }
+    if (id === ("superList" || "proficiencyList" || "positiveList" || "negativeList" || "neutralList")) {
+        var list = generalToList(id);
+        document.getElementById(id).innerText = randomLine(list);
+    }
 
+    else {
+        var list = listFunction;
 
-
-
-function newSetOfThreeElements(elementOne, elementTwo, elementThree) {
+        document.getElementById(id).innerText = randomLine(list);
+    }
+}
+function getPersonality() {
     let traitList = Array();
-    traitList.push(elementOne);
-    traitList.push(elementTwo);
-    traitList.push(elementThree);
+    let maxChance = 100;
+    for (let i = 0; i < 3; i++) {
+        var chance = Math.random() * maxChance;
+        switch (true) {
+            case chance < 18:
+                var newID = "neutralList";
+                break;
+            case chance < 54:
+                var newID = "positiveList";
+                break;
+            case 53 < chance:
+                var newID = "negativeList";
+                break;
+        }
+        var list = generalToList(newID);
+        traitList.push(randomLine(list));
+    }
     var string = "";
     traitList.forEach(function (element) {
-        string += element;
+        string += element + " ";
+    });
+    return string;
+}
+function getProficiencies(id) {
+    let traitList = Array();
+    for (let i = 0; i < 3; i++) {
+        var list = generalToList(id);
+        traitList.push(randomLine(list));
+    }
+    var string = "";
+    traitList.forEach(function (element) {
+        string += element + " ";
     });
     return string;
 }
 
-function newExperiance() {
+export function newHumanoidCharacter() {
+    document.getElementById("CharacterSheet").style.display = "block";
+    let character = "humanoid";
+    //sets 100% var
     let maxChance = 100;
-    var chance = Math.random() * maxChance;
-    if (chance > 30) {
-        return randomLine("files/characterCreationFiles/experiance.txt")
+    let lowChance = 70;
+    let lowerChance = 80;
+    let extremelyLowChance = 90;
+    //species
+    var species = returnSpecies(character, "speciesList");
+    //name
+    returnElement(character, species, "nameList");
+    //type
+    returnElement(character, species, "typeList");
+    //mutated
+    var mutatedChance = Math.random() * maxChance;
+    if (mutatedChance > extremelyLowChance) {
+        document.getElementById("mutated").innerText = "true";
     }
     else {
-        return "empty\n";
+        document.getElementById("mutated").innerText = "false";
     }
+    //superpower
+    let hasSuperpowers = false;
+    var superChance = Math.random() * maxChance;
+    if (superChance > 10) {
+        returnElement(character, species, "superList");
+        hasSuperpowers = true;
+    }
+    else {
+        document.getElementById("superList").innerText = "false";
+    }
+    //superpower2
+    if (hasSuperpowers) {
+        let superChance2 = Math.random() * maxChance;
+        if (superChance2 > lowerChance) {
+            returnElement(character, species, "superList2");
+        }
+        else {
+            document.getElementById("superList2").innerText = "false";
+        }
+    }
+    else {
+        document.getElementById("superList2").innerText = "false";
+    }
+    //age
+    document.getElementById("sexualityList").innerText = returnElement(character, species, "ageRange");
+    //sex
+    returnElement(character, species, "sexesList");
+    //sexualities
+    var sexualityChance = Math.random() * maxChance;
+    if (sexualityChance > lowChance) {
+        document.getElementById("sexualityList").innerText = "Heterosexual";
+    }
+    else {
+        returnElement(character, species, "sexualityList");
+    }
+    //skills
+    returnElement(character, species, "proficiencyList");
+    //personalities
+    returnElement(character, species, "personality");
+    //ideology
+    returnElement(character, species, "ideologyList");
+    
 }
 
-function newPersonality() {
-    let maxChance = 100;
-    var chance = Math.random() * maxChance;
-    switch (true) {
-        case chance < 18:
-            var path = "files/characterCreationFiles/personalityTraits/Neutral.txt";
-            break;
-        case chance < 54:
-            var path = "files/characterCreationFiles/personalityTraits/Positive.txt";
-            break;
-        case 53 < chance:
-            var path = "files/characterCreationFiles/personalityTraits/Negative.txt";
-            break;
-    }
-    return randomLine(path);
-}
+
+
+
+
+
 
 
 
